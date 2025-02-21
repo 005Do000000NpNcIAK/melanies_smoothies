ad224aecd27926b58e6f3e5b433288741e0bb879
005Do000000NpNcIAK <gbksnowflake.001@gmail.com>
2025-02-21 18:41:58 +0530

    Update streamlit_app.py
    corrected the code for col('SEARCH_ON')

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,26 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie :cup_with_straw:")
-st.write(
-    """Choose the fruits you want in your custom, *Smoothie!*.
-    """)
+st.write("""Choose the fruits you want in your custom, *Smoothie!*. """)
 
 name_on_order = st.text_input('Name of Smoothie:')
 st.write('The name on your Smoothie will be :',name_on_order)
 
-#session = get_active_session()
-
 cnx = st.connection("snowflake")
 session = cnx.session()
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col(SEARCH_ON))
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
+
 ingredeient_list = st.multiselect('Choose upto 5 ingredients:'
                                   ,my_dataframe
                                   ,max_selections = 5
                                  )
-
 if ingredeient_list:    
     ingredients_string = ''
     for fruit_choosen in ingredeient_list:
